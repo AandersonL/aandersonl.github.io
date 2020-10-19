a9ecf6230d4b38e55efb005517e497dfe660e072
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-19 15:09:42 +0000

    Deploy AandersonL/aandersonl.github.io to github.com/AandersonL/aandersonl.github.io.git:master

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Beating Console challenge from Hackthebox",
+        "excerpt":"This challenge from hackthebox, give you an address with a running PHP application, when you open the web page, you will notice a phpinfo() page with: Your IP is 10.255.0.2 Make sure to load php-console in order to be prompted for a password The first thing that came in my...","categories": ["cybersec"],
+        "tags": [],
+        "url": "https://aandersonl.github.io/cybersec/Hacking-PHP-Console-Hackthebox/",
+        "teaser": null
+      },{
+        "title": "Fireshell CTF 2020 Write-ups",
+        "excerpt":"Write-ups from Fireshell CTF Rev (500 Points) Rev is a simple reverse enginner task, we need to undestand the cryptographic function and reverse it. Reversing The binary provided to us was stripped, so we did’t have any debug symbols, to find our main funcion quicly as possible i used Radare2...","categories": ["cybersec","write-up"],
+        "tags": [],
+        "url": "https://aandersonl.github.io/cybersec/write-up/Fireshell-CTF-2020/",
+        "teaser": null
+      },{
+        "title": "Using bipartite Graphs to detect Malware campaigns",
+        "excerpt":"One of the greatest problems in mapping threats today, is detect from where it’s coming all kinds of threats, if is from the same group, same person or even the same governament. In order to group everything up and make things more clear, we can use a lot of data...","categories": ["cybersec","programming"],
+        "tags": [],
+        "url": "https://aandersonl.github.io/cybersec/programming/bipartite-graph-for-threats/",
+        "teaser": "https://aandersonl.github.io/assets/images/bipartite_graph/bipartite.jpg"
+      },{
+        "title": "Reverse shells: Learn by writing",
+        "excerpt":"A shell, its probably the goal of every security researcher, almost every critical flaws or 0-Day exploits give us a way to run a remote command inside a system (RCE - Remote Code Execution). Before copy and paste, we need to have a solid knowledge of what we are trying...","categories": ["programming","cybersec","powershell"],
+        "tags": [],
+        "url": "https://aandersonl.github.io/programming/cybersec/powershell/reverse-shells-write-and-hunt-part-1/",
+        "teaser": null
+      }]
