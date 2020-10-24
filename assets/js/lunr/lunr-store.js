199f0df12b81c00c87129bb7aaecd1bd69aca056
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-24 04:49:29 +0000

    Deploy AandersonL/aandersonl.github.io to github.com/AandersonL/aandersonl.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,24 +2,42 @@ var store = [{
         "title": "Beating Console challenge from Hackthebox",
         "excerpt":"This challenge from hackthebox, give you an address with a running PHP application, when you open the web page, you will notice a phpinfo() page with: Your IP is 10.255.0.2 Make sure to load php-console in order to be prompted for a password The first thing that came in my...","categories": ["cybersec","write-up"],
         "tags": [],
-        "url": "https://aandersonl.github.io/cybersec/write-up/Hacking-PHP-Console-Hackthebox/",
+        "url": "https://aandersonl.github.io/Hacking-PHP-Console-Hackthebox/",
         "teaser": null
       },{
         "title": "Fireshell CTF 2020 Write-ups",
         "excerpt":"Rev (500 Points) Rev is a simple reverse enginner task, we need to undestand the cryptographic function and reverse it. Reversing The binary provided to us was stripped, so we did’t have any debug symbols, to find our main funcion quicly as possible i used Radare2 framework to it, so...","categories": ["cybersec","write-up"],
         "tags": [],
-        "url": "https://aandersonl.github.io/cybersec/write-up/Fireshell-CTF-2020/",
+        "url": "https://aandersonl.github.io/Fireshell-CTF-2020/",
         "teaser": null
       },{
         "title": "Using bipartite Graphs to detect Malware campaigns",
-        "excerpt":"One of the greatest problems in mapping threats today, is detect from where it’s coming all kinds of threats, if is from the same group, same person or even the same governament. In order to group everything up and make things more clear, we can use a lot of data...","categories": ["cybersec","programming"],
+        "excerpt":"One of the greatest problems in mapping threats today, is detect from where it’s came, if is from the same group, same person or even from the same governament. In order to group everything up and make things more clear, we can use a lot of data structures for that,...","categories": ["cybersec","programming"],
         "tags": [],
-        "url": "https://aandersonl.github.io/cybersec/programming/bipartite-graph-for-threats/",
+        "url": "https://aandersonl.github.io/bipartite-graph-for-threats/",
         "teaser": null
       },{
         "title": "Reverse shells: Learn by writing",
         "excerpt":"A shell, its probably the goal of every security researcher, almost every critical flaws or 0-Day exploits give us a way to run a remote command inside a system (RCE - Remote Code Execution). Before copy and paste, we need to have a solid knowledge of what we are trying...","categories": ["programming","cybersec","powershell"],
         "tags": [],
-        "url": "https://aandersonl.github.io/programming/cybersec/powershell/reverse-shells-write-and-hunt-part-1/",
+        "url": "https://aandersonl.github.io/reverse-shells-write-and-hunt-part-1/",
+        "teaser": null
+      },{
+        "title": "The Flare 7th AutoIT challenge",
+        "excerpt":"The 7th challenge of Flare-On CTF, give to us a QR code generator software that are fully obfuscated, the fun part of this challenge was interpret, analyse and deobfuscate core components of the software and then, make it give us the flag. Why this is useful ? Obfuscated script In...","categories": ["cybersec","write-up","malware","flare7"],
+        "tags": [],
+        "url": "https://aandersonl.github.io/Code-it-flare-7/",
+        "teaser": null
+      },{
+        "title": "VBA Stomping: The macro hidden in plain sight",
+        "excerpt":"At Flare-on 7th there was a very interesting malware analysis challenge that envolved a very unique hide technique for malicious Macros. This technique is called VBA Stomp, this works by hiding the real source code compiled in P-Code, the “bytecode” used in macros, and then making the visible source code...","categories": ["cybersec","write-up","flare7"],
+        "tags": [],
+        "url": "https://aandersonl.github.io/VBA-Stomping-flare/",
+        "teaser": null
+      },{
+        "title": "Data exfiltration: From shellcode to flag",
+        "excerpt":"At Flare 7th edition, my favorite challenge was re_crowd, this challenge was really close to a very real world scenario, with just an pcap we are able to understand how the company was attacked, what exploit was used and what shellcode was sent, literally: Why, what and how ? Why...","categories": ["cybersec","write-up","shellcode","flare7"],
+        "tags": [],
+        "url": "https://aandersonl.github.io/Retrieving-the-exfiltrated-flag-flareon7/",
         "teaser": null
       }]
